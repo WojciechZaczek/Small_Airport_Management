f77014ffb58b0880ba49d85c425ba28db25e16d9
github-WojciechZaczek <wojciech.zaczek84@gmail.com>
2023-07-01 12:27:55 +0200

    build: Airport model modified

--- airport/models.py
+++ airport/models.py
@@ -4,5 +4,12 @@ from django.db import models
 class Airport(models.Model):
     name = models.CharField(max_length=30)
     address = models.TextField(max_length=200)
-    type = models.TextField(max_length=30)
+    contact = models.TextField(max_length=200)
+    types_of_traffic_permitted = models.TextField(max_length=200)
+    radio = models.FloatField()
+    elevation = models.IntegerField()
+    co_ordinates = models.TextField()
     description = models.TextField(max_length=300)
+    length = models.IntegerField()
+    width = models.IntegerField()
+    square_meters = models.IntegerField()
